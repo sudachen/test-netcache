38d9f2b3ca91e5163da069a452f1dbf28b7e029f
Alexey Sudachen <alexey@sudachen.name>
2020-06-09 09:41:04 -0400

    updated

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/sudachen/net-cache
 
 go 1.13
+
+require (
+	github.com/google/go-cmp v0.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gotest.tools v2.2.0+incompatible
+)
--- netcache/netcache_test.go
+++ netcache/netcache_test.go
@@ -2,9 +2,41 @@ package netcache
 
 import (
 	"fmt"
+	"gotest.tools/assert"
 	"testing"
 	"time"
 )
+
+const ConnectionTimeout = time.Second * 5
+
+func isready(nc *NetCache, peers ...PeerAddress) bool {
+	for _,p := range peers {
+		if c,ok := nc.cache[p]; ok {
+			select {
+				case _, ok = <- c.ready:
+				default:
+					return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func isnotready(nc *NetCache, peers ...PeerAddress) bool {
+	for _,p := range peers {
+		if c,ok := nc.cache[p]; ok {
+			select {
+			case _, ok = <- c.ready:
+				return false
+			default:
+				break
+			}
+		}
+	}
+	return true
+}
 
 type TestConnectionFactory struct {}
 type TestConnection struct {peer PeerAddress}
@@ -15,7 +47,7 @@ func (f *TestConnectionFactory) New(peer PeerAddress) Connection {
 
 func (c *TestConnection) Open() {
 	fmt.Println("open ", c.peer)
-	time.Sleep(5*time.Second)
+	time.Sleep(ConnectionTimeout)
 	fmt.Println("opened ", c.peer)
 }
 
@@ -25,10 +57,30 @@ func (c *TestConnection) Close() {
 
 func Test_OneConnection(t *testing.T) {
 	nc := New(&TestConnectionFactory{})
+	nc.GetConnection(1)
+	nc.Shutdown()
+}
+
+func Test_ConcurrentConnections(t *testing.T) {
+	nc := New(&TestConnectionFactory{})
 	go func() {nc.GetConnection(1)}()
-	nc.GetConnection(2)
-	go func() {nc.GetConnection(3)}()
-	time.Sleep(300*time.Millisecond)
+	go func() {nc.GetConnection(2)}()
+	time.Sleep(ConnectionTimeout+time.Second)
+	assert.Assert(t,isready(nc,1,2))
+	nc.Shutdown()
+}
+
+func Test_Callback(t *testing.T) {
+	nc := New(&TestConnectionFactory{})
+	go func() {nc.GetConnection(1)} ()
+	go func() {nc.GetConnection(2)} ()
+	go func() {nc.GetConnection(3)} ()
+	time.Sleep(time.Millisecond*100)
 	nc.OnNewRemoteConnection(2,&TestConnection{2})
+	assert.Assert(t,isready(nc,2))
+	nc.OnNewRemoteConnection(1,&TestConnection{1})
+	assert.Assert(t,isready(nc,1))
+	assert.Assert(t,isnotready(nc,3))
+	fmt.Println("shutdown")
 	nc.Shutdown()
 }
